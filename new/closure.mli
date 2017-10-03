3f69efb1fc5bd624a68e528fea011dc4a96c6b8a
touyou <fujiyou1121@gmail.com>
2017-10-03 22:35:06 +0900

    add closure

---
+++ new/closure.mli
@@ -0,0 +1,36 @@
+type closure = { entry : Id.label; actual_free_var : Id.t list }
+type t =
+  | Unit
+  | Int of int
+  | Float of float
+  | Neg of Id.t
+  | Add of Id.t * Id.t
+  | Sub of Id.t * Id.t
+  | Mul of Id.t * Id.t
+  | Div of Id.t * Id.t
+  | FNeg of Id.t
+  | FAdd of Id.t * Id.t
+  | FSub of Id.t * Id.t
+  | FMul of Id.t * Id.t
+  | FDiv of Id.t * Id.t
+  | IfEq of Id.t * Id.t * t * t
+  | IfLE of Id.t * Id.t * t * t
+  | Let of (Id.t * Type.t) * t * t
+  | Var of Id.t
+  | MakeCls of (Id.t * Type.t) * closure * t
+  | AppCls of Id.t * Id.t list
+  | AppDir of Id.label * Id.t list
+  | Tuple of Id.t list
+  | LetTuple of (Id.t * Type.t) list * Id.t * t
+  | Get of Id.t * Id.t
+  | Put of Id.t * Id.t * Id.t
+  | ExtArray of Id.label
+type fun_def = { name : Id.label * Type.t;
+                 args : (Id.t * Type.t) list;
+                 formal_free_var : (Id.t * Type.t) list;
+                 body : t }
+type prog = Prog of fun_def list * t
+
+val log : t -> unit
+val free_var : t -> MiniSet.t
+val main : KNormal.t -> prog
--- new/kNormal.ml
+++ new/kNormal.ml
@@ -25,7 +25,6 @@ type t =
   | ExtArray of Id.t
   | ExtFunApp of Id.t * Id.t list
 and fun_def = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
-;;
 
 (* デバック用 TODO: エラー出力にするかも  *)
 let rec log elem =
@@ -43,13 +42,13 @@ let rec log elem =
   | Div(e1, e2) | FDiv(e1, e2) -> print_string "("; Id.log e1; print_string " / ";
                                   Id.log e2; print_string ")"
   | IfEq(e1, e2, e3, e4) -> print_string "if "; Id.log e1; print_string " == "; Id.log e2; print_string " then"; print_newline ();
-                            Id.log e3; print_newline ();
+                            log e3; print_newline ();
                             print_string "else"; print_newline ();
-                            Id.log e4
+                            log e4
   | IfLE(e1, e2, e3, e4) -> print_string "if "; Id.log e1; print_string " <= "; Id.log e2; print_string " then"; print_newline ();
-                            Id.log e3; print_newline ();
+                            log e3; print_newline ();
                             print_string "else"; print_newline ();
-                            Id.log e4
+                            log e4
   | Let((id, typ), e1, e2) -> print_string "let "; Id.log id; print_string ": "; Type.log typ; print_string " ="; print_newline ();
                               log e1; print_string " in"; print_newline();
                               log e2
@@ -106,72 +105,124 @@ let insert_let (e, t) k =
       Let((x, t), e, e'), t'
 
 (* k正規化 *)
-let rec main env = function
+let rec normalize env = function
   | Syntax.Unit -> Unit, Type.Unit
   | Syntax.Bool(b) -> Int(if b then 1 else 0), Type.Int
   | Syntax.Int(i) -> Int(i), Type.Int
   | Syntax.Float(d) -> Float(d), Type.Float
-  | Syntax.Not(e) -> main env (Syntax.If(e, Syntax.Bool(false), Syntax.Bool(true)))
+  | Syntax.Not(e) -> normalize env (Syntax.If(e, Syntax.Bool(false), Syntax.Bool(true)))
   | Syntax.Neg(e) ->
-      insert_let (main env e)
+      insert_let (normalize env e)
         (fun x -> Neg(x), Type.Int)
   | Syntax.Add(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> Add(x, y), Type.Int))
   | Syntax.Sub(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> Sub(x, y), Type.Int))
   | Syntax.Mul(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> Mul(x, y), Type.Int))
   | Syntax.Div(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> Div(x, y), Type.Int))
   | Syntax.FAdd(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> FAdd(x, y), Type.Float))
   | Syntax.FSub(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> FSub(x, y), Type.Float))
   | Syntax.FMul(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> FMul(x, y), Type.Float))
   | Syntax.FDiv(e1, e2) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y -> FDiv(x, y), Type.Float))
   | Syntax.Eq _ | Syntax.LE _ as cmp ->
-      main env (Syntax.If(cmp, Syntax.Bool(true), Syntax.Bool(false)))
-  | Sytax.If(Syntax.Not(e1), e2, e3) -> main env (Syntax.If(e1, e3, e2))
+      normalize env (Syntax.If(cmp, Syntax.Bool(true), Syntax.Bool(false)))
+  | Sytax.If(Syntax.Not(e1), e2, e3) -> normalize env (Syntax.If(e1, e3, e2))
   | Syntax.If(Syntax.Eq(e1, e2), e3, e4) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y ->
-               let e3', t3 = main env e3 in
-               let e4', t4 = main env e4 in
+               let e3', t3 = normalize env e3 in
+               let e4', t4 = normalize env e4 in
                IfEq(x, y, e3', e4'), t3))
   | Syntax.If(Syntax.LE(e1, e2), e3, e4) ->
-      insert_let (main env e1)
-        (fun x -> insert_let (main env e2)
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
             (fun y ->
-               let e3', t3 = main env e3 in
-               let e4', t4 = main env e4 in
+               let e3', t3 = normalize env e3 in
+               let e4', t4 = normalize env e4 in
                IfLE(x, y, e3', e4'), t3))
-  | Syntax.If(e1, e2, e3) -> main env (Syntax.If(Syntax.Eq(e1, Syntax.Bool(false)), e3, e2))
+  | Syntax.If(e1, e2, e3) -> normalize env (Syntax.If(Syntax.Eq(e1, Syntax.Bool(false)), e3, e2))
   | Syntax.Let((x, t), e1, e2) ->
-      let e1', t1 = main env e1 in
-      let e2', t2 = main (MiniMap.add x t env) e2 in
+      let e1', t1 = normalize env e1 in
+      let e2', t2 = normalize (MiniMap.add x t env) e2 in
       Let((x, t), e1', e2), t2
   | Synax.Var(x) when MiniMap.mem x env -> Var(x), MiniMap.find x env
   | Syntax.Var(x) ->
       (match MiniMap.find x !Typing.ext_env with
       | Type.Array(_) as t -> ExtArray x, t
       | _ -> failwith (Printf.sprintf "external variable %s does not have an array type" x))
-  | 
+  | Syntax.LetRec({ Syntax.name = (x, t); Syntax.args = yts; Syntax.body = e1 }, e2) ->
+      let env' = MiniMap.add x t env in
+      let e2', t2 = normalize env' e2 in
+      let e1', t1 = normalize (MiniMap.add_list yts env') e1 in
+      LetRec({ name = (x, t); args = yts; body = e1' }, e2'), t2
+  | Syntax.App(Syntax.Var(f), e2s) when not (MiniMap.mem f env) ->
+      (match MiniMap.find f !Typing.extenv with
+      | Type.Fun(_, t) ->
+          let rec bind xs = function
+            | [] -> ExtFunApp(f, xs), t
+            | e2 :: e2s ->
+                insert_let (normalize env e2)
+                  (fun x -> bind (xs @ [x]) e2s) in
+          bind [] e2s)
+      | _ -> assert false)
+  | Syntax.Tuple(es) ->
+      let rec bind xs ts = function
+        | [] -> Tuple(xs), Type.Tuple(ts)
+        | e :: es ->
+            let _, t as g_e = normalize env e in
+            insert_let g_e
+              (fun x -> bind (xs @ [x]) (ts @ [t]) es) in
+      bind [] [] es
+  | Syntax.LetTuple(xts, e1, e2) ->
+      insert_let (normalize env e1)
+        (fun y ->
+           let e2', t2 = normalize (MiniMap.add_list xts env) e2 in
+           LetTuple(xts, y, e2'), t2)
+  | Syntax.Array(e1, e2) ->
+      insert_let (normalize env e1)
+        (fun x ->
+           let _, t2 as g_e2 = normalize env e2 in
+           insert_let g_e2
+             (fun y ->
+                let l =
+                  match t2 with
+                  | Type.Float -> "create_float_array"
+                  | _ -> "create_array" in
+                ExtFunApp(l, [x; y]), Type.Array(t2)))
+  | Syntax.Get(e1, e2) ->
+      (match normalize env e1 with
+      | _, Type.Array(t) as g_e1 ->
+          insert_let g_e1
+            (fun x -> insert_let (normalize env e2)
+                (fun y -> Get(x, y), t))
+      | _ -> assert false)
+  | Syntax.Put(e1, e2, e3) ->
+      insert_let (normalize env e1)
+        (fun x -> insert_let (normalize env e2)
+            (fun y -> insert_let (normalize env e3)
+                (fun z -> Put(x, y, z), Type.Unit)))
+
+let main e = fst (normalize MiniMap.empty e)
