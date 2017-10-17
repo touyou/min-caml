4b6bc3efc0cf4e1d806efb808d15f3fc716de7bf
touyou <fujiyou1121@gmail.com>
2017-10-17 16:20:57 +0900

    modify or bug

--- emit.ml
+++ emit.ml
@@ -82,7 +82,7 @@ and assemble_inst oc = function
     Printf.fprintf oc "%s" s
   | NonTail(x), Mr(y) when x = y -> ()
   (* or %r2, %r1, %r2 *)
-  | NonTail(x), Mr(y) -> Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" x y x x y
+  | NonTail(x), Mr(y) -> Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" y x y x y
   (* neg %r1, %r2 *)
   | NonTail(x), Neg(y) -> Printf.fprintf oc "\tneg\t%s, %s\n" x y
   (* add %r1, %r2, %r3 *)
@@ -257,7 +257,7 @@ and assemble_inst oc = function
     Printf.fprintf oc "\taddi\t%s, %s, %d\t# subi\n" reg_stack_p reg_stack_p (-ss);
     Printf.fprintf oc "\tlwz\t%s, %d(%s)\n" reg_tmp (ss - 4) reg_stack_p;
     if List.mem a all_regs && a <> regs.(0) then
-      Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" a regs.(0) a a regs.(0)
+      Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" regs.(0) a regs.(0) a regs.(0)
     else if List.mem a all_fregs && a <> fregs.(0) then
       Printf.fprintf oc "\tfmr\t%s, %s\n" a fregs.(0);
     Printf.fprintf oc "\tmtspr\t8, %s\t# mtlr\n" reg_tmp
@@ -271,7 +271,7 @@ and assemble_inst oc = function
     Printf.fprintf oc "\taddi\t%s, %s, %d\t# subi\n" reg_stack_p reg_stack_p (-ss);
     Printf.fprintf oc "\tlwz\t%s, %d(%s)\n" reg_tmp (ss - 4) reg_stack_p;
     if List.mem a all_regs && a <> regs.(0) then
-      Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" a regs.(0) a a regs.(0)
+      Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" regs.(0) a regs.(0) a regs.(0)
     else if List.mem a all_fregs && a <> fregs.(0) then
       Printf.fprintf oc "\tfmr\t%s, %s\n" a fregs.(0);
     Printf.fprintf oc "\tmtspr\t8, %s\t# mtlr\n" reg_tmp
@@ -304,7 +304,7 @@ and assemble_args oc x_reg_cl ys zs =
       (0, x_reg_cl)
       ys in
   List.iter
-    (fun (y, r) -> Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" r y r r y)
+    (fun (y, r) -> Printf.fprintf oc "\tor\t%s, %s, %s\t# mr %s, %s\n" y r y r y)
     (shuffle reg_swap yrs);
   let (d, zfrs) =
     List.fold_left
