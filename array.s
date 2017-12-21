4728c84804dc9c1919453525c846750890affa7e
touyou <fujiyou1121@gmail.com>
2017-12-21 20:20:27 +0900

    remove comm

--- array.s
+++ array.s
@@ -1,35 +1,3 @@
-	.data
-	.align 	8
-  .comm min_caml_n_objects, 4
-#  .comm min_caml_objects, 240
-  .comm min_caml_screen, 24
-  .comm	min_caml_viewpoint, 24
-  .comm min_caml_light, 24
-#  .comm min_caml_beam, 8
-#  .comm min_caml_and_net, 200
-#  .comm min_caml_or_net, 4
-  .comm min_caml_solver_dist, 8
-  .comm min_caml_intsec_rectside, 4
-#  .comm min_caml_tmin, 8
-  .comm min_caml_intersection_point, 24
-  .comm min_caml_intersected_object_id, 4
-  .comm min_caml_nvector, 24
-  .comm min_caml_texture_color, 24
-  .comm min_caml_diffuse_ray, 24
-  .comm min_caml_rgb, 24
-  .comm min_caml_image_size, 8
-  .comm min_caml_image_center, 8
-  .comm min_caml_scan_pitch, 8
-  .comm min_caml_startp, 24
-  .comm min_caml_startp_fast, 24
-  .comm min_caml_screenx_dir, 24
-  .comm min_caml_screeny_dir, 24
-  .comm min_caml_screenz_dir, 24
-  .comm min_caml_ptrace_dirvec, 24
-#  .comm min_caml_dirvecs, 20
-#  .comm min_caml_light_dirvec, 264
-#  .comm min_caml_reflections, 720
-  .comm min_caml_n_reflections, 4
 #	create_array
 	.text
 	.align	2
@@ -58,5 +26,5 @@ create_float_array_loop:
 create_float_array_cont:
 	stfs	%f0, 0(%r4)
 	addi	%r5, %r5, -1  # subi	%r5, %r5, 1
-	addi	%r4, %r4, 8
+	addi	%r4, %r4, 4
 	b	create_float_array_loop
